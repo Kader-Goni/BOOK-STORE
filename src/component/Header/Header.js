49120c3421454a4ce8cf08dd8edf6404b9848db5
Kader-Goni <kadergoni35@gmail.com>
2022-03-27 03:41:18 +0600

    components-edit

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='mt-3'>
             <h1 className='text-center bg-info fw-bold text-white display-4'>BOOK STORE</h1>
-            <h3 className='text-center text-primary display-6'>Choose Your Book 02</h3>
+            <h3 className='text-center text-primary display-6'>Choose Your Book</h3>
         </div>
     );
 };
